8f7a093ef5cec9d9790c299b8605d72dacb6f2a8
Amir Karbasi <amir@karbasi.dev>
2026-01-08 22:08:53 -0500

    Retrieve proper json based on full path

--- src/services/utils/AppConfig.ts
+++ src/services/utils/AppConfig.ts
@@ -58,7 +58,11 @@ export default class AppConfig {
     }
 
     // Fall back on retrieving the config from the server
-    const prodAppConfigJson = await fetch("tri-app-config.json");
+    // Use pathname to ignore hash routing, and ensure trailing slash
+    const pathname = window.location.pathname.endsWith("/")
+      ? window.location.pathname
+      : window.location.pathname + "/";
+    const prodAppConfigJson = await fetch(pathname + "tri-app-config.json");
 
     if (prodAppConfigJson.ok) {
       const prodAppProperties: WebAppProperties =
